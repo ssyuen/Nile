9f00cf5073ec76716d10a6258830582b4f49e4f7
ssyuen <samuel.s.yuen@gmail.com>
2020-02-28 07:29:18 -0500

    struct changed, front-end done

--- app.py
+++ app.py
@@ -10,6 +10,66 @@ app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql = MySQL()
 mysql.init_app(app)
 
+
+"""
+This doubles as the browinsg page and the landing page.
+"""
 @app.route('/')
 def landing_page():
-    return 'working'
+    return render_template('browse.html')
+
+@app.route('/login.html')
+def login():
+    return render_template('login.html')
+
+@app.route('/reg.html')
+def reg():
+    return render_template('reg.html')
+
+@app.route('/shoppingcart.html')
+def shopping_cart():
+    return render_template('shoppingcart.html')
+
+@app.route('/user/billingaddress.html')
+def billing_address():
+    return render_template('./user pages/billingaddress.html')
+
+@app.route('/user/checkout.html')
+def checkout():
+    return render_template('./user pages/checkout.html')
+
+@app.route('/user/orderhist.html')
+def order_history():
+    return render_template('./user pages/orderhist.html')
+
+@app.route('/user/paymentinfo.html')
+def payment_info():
+    return render_template('./user pages/paymentinfo.html')
+
+@app.route('/user/profile.html')
+def profile():
+    return render_template('./user pages/profile.html')
+
+@app.route('/admin/add_books.html')
+def profile():
+    return render_template('./user pages/add_books.html')
+
+@app.route('/admin/add_promo.html')
+def profile():
+    return render_template('./user pages/add_promo.html')
+
+@app.route('/admin/admin.html')
+def profile():
+    return render_template('./user pages/admin.html')
+
+@app.route('/admin/manage_books.html')
+def profile():
+    return render_template('./user pages/manage_books.html')
+
+@app.route('/admin/manage_promo.html')
+def profile():
+    return render_template('./user pages/manage_promo.html')
+
+@app.route('/admin/manage_users.html')
+def profile():
+    return render_template('./user pages/manage_users.html')
