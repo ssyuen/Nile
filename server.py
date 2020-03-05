d4af03f631389fd12b49e03557bf19899c4e6bab
ssyuen <samuel.s.yuen@gmail.com>
2020-03-04 23:52:34 -0500

    formatting

--- server.py
+++ server.py
@@ -1,3 +1,5 @@
+from routes.user import routes as user_routes
+from routes.admin import routes as admin_routes
 from flask import Flask
 from flaskext.mysql import MySQL
 import random as rand
@@ -18,16 +20,12 @@ app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
 app.config['MYSQL_DATABASE_DB'] = 'nile_db'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 
-app.secret_key = str(rand.randint(0,100))
+app.secret_key = str(rand.randint(0, 100))
 
 mysql = MySQL()
 mysql.init_app(app)
 conn = mysql.connect()
 
-
-
-from routes.admin import routes as admin_routes
-from routes.user import routes as user_routes
 
 app.register_blueprint(admin_routes.admin_bp)
 app.register_blueprint(user_routes.user_bp)
