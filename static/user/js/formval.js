1cb34edad9a76deb9ed37f77eb5e04cf43840aaa
manu-p-1 <mjp45770@uga.edu>
2020-03-15 13:26:01 -0400

    Added form validation JS

---
+++ static/user/js/formval.js
@@ -0,0 +1,22 @@
+const fName = document.getElementById("inputFirstName");
+const lName = document.getElementById("inputLastName");
+const email = document.getElementById("inputEmail");
+const pass = document.getElementById("inputPassword");
+const passConf = document.getElementById("inputConfirmPassword");
+
+
+form.addEventListener('submit', (e) => {
+
+    let messages = []
+
+    if(fName.length <= 1){
+
+    }
+
+    if(lName.length <= 1){
+
+    }
+
+    e.preventDefault();
+
+})
