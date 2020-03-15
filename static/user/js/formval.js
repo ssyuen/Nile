53814b613869b78571eb9f0f78f63704071f9299
Manu Puduvalli <28782438+manu-p-1@users.noreply.github.com>
2020-03-15 14:44:28 -0400

    Basic formval skeleton added

--- static/user/js/formval.js
+++ static/user/js/formval.js
@@ -1,22 +1,81 @@
-const fName = document.getElementById("inputFirstName");
-const lName = document.getElementById("inputLastName");
+const form = document.getElementById("regForm");
+const fName = document.getElementById("inputFirstname");
+const lName = document.getElementById("inputLastname");
 const email = document.getElementById("inputEmail");
 const pass = document.getElementById("inputPassword");
 const passConf = document.getElementById("inputConfirmPassword");
 
-
 form.addEventListener('submit', (e) => {
 
-    let messages = []
+});
 
-    if(fName.length <= 1){
+fName.addEventListener('input', (e) => {
 
+    console.log(fName);
+    let val = fName.value;
+
+    console.log(val.length);
+    if (val.length >= 1) {
+        console.log(true);
+    } else {
+        console.log(false);
+    }
+});
+
+lName.addEventListener('input', (e) => {
+
+    console.log(lName);
+    let val = lName.value;
+
+    console.log(val.length);
+    if (val.length >= 2) {
+        console.log(true);
+    } else {
+        console.log(false);
+    }
+});
+
+email.addEventListener('input', (e) => {
+
+    console.log(email);
+    let val = email.value;
+
+    console.log(val.length);
+    if (val.length <= 1) {
+        console.log(false);
+    } else {
+        console.log(true);
     }
 
-    if(lName.length <= 1){
+});
 
+pass.addEventListener('input', (e) => {
+
+    console.log(fName);
+    let val = fName.value;
+
+    console.log(val.length);
+    if (val.length <= 1) {
+        console.log(false);
+    } else {
+        console.log(true);
     }
 
-    e.preventDefault();
+});
 
-})
+
+passConf.addEventListener('input', (e) => {
+
+    console.log(fName);
+    let pConf = pass.value;
+    let pConfVal = passConf.value;
+
+    if (pConf === pConfVal) {
+        console.log(true);
+    } else {
+        console.log(false);
+    }
+
+});
+
+
