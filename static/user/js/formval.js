8407e27fa3943cfbf429cd748730a32bb991f8b2
Manu Puduvalli <28782438+manu-p-1@users.noreply.github.com>
2020-03-15 18:01:01 -0400

    Added validation for Required Fields ONLY

--- static/user/js/formval.js
+++ static/user/js/formval.js
@@ -5,77 +5,122 @@ const email = document.getElementById("inputEmail");
 const pass = document.getElementById("inputPassword");
 const passConf = document.getElementById("inputConfirmPassword");
 
+const invalidPass = `
+    <div class="error-message" id="invalidPass">
+        <small class="text-danger">
+          Password be more than 8 or more characters long, contain at least 1 uppercase character, 1 lowercase character, and 1 number.
+        </small>
+    </div>
+`;
+
+const invalidPassConf = `
+    <div class="error-message" id="invalidPassConf">
+        <small class="text-danger">
+          Password's do not match.
+        </small>
+    </div>
+`;
+
+const invalidEmail = `
+    <div class="error-message" id="invalidEmail">
+        <small class="text-danger">
+          Email is not valid.
+        </small>
+    </div>
+`;
+
+const invalidFName = `
+    <div class="error-message" id="invalidFName">
+        <small class="text-danger">
+          Firstname must be more than 1 character.
+        </small>
+    </div>
+`;
+
+const invalidLName = `
+    <div class="error-message" id="invalidLName">
+        <small class="text-danger">
+          Lastname must be more than 2 characters.
+        </small>
+    </div>
+`;
+
 form.addEventListener('submit', (e) => {
 
 });
 
-fName.addEventListener('input', (e) => {
+['input', 'focusin'].forEach((evt) => {
 
-    console.log(fName);
-    let val = fName.value;
+    fName.addEventListener(evt, (e) => {
+        console.log(lName);
+        let val = fName.value;
+        validate(fName, invalidFName, '#invalidFName', val.length >= 1);
+    });
 
-    console.log(val.length);
-    if (val.length >= 1) {
-        console.log(true);
-    } else {
-        console.log(false);
-    }
-});
+    lName.addEventListener(evt, (e) => {
+        console.log(lName);
+        let val = lName.value;
+        validate(lName, invalidLName, '#invalidLName', val.length >= 2);
+    });
 
-lName.addEventListener('input', (e) => {
+    email.addEventListener(evt, (e) => {
+        validate(email, invalidEmail, '#invalidEmail', emailConstraint());
+    });
 
-    console.log(lName);
-    let val = lName.value;
+    pass.addEventListener(evt, (e) => {
+        validate(pass, invalidPass, '#invalidPass', validateConstraint());
+        validate(passConf, invalidPassConf, '#invalidPassConf', confirmConstraint());
+    });
 
-    console.log(val.length);
-    if (val.length >= 2) {
-        console.log(true);
-    } else {
-        console.log(false);
-    }
-});
-
-email.addEventListener('input', (e) => {
-
-    console.log(email);
-    let val = email.value;
-
-    console.log(val.length);
-    if (val.length <= 1) {
-        console.log(false);
-    } else {
-        console.log(true);
-    }
-
-});
-
-pass.addEventListener('input', (e) => {
-
-    console.log(fName);
-    let val = fName.value;
-
-    console.log(val.length);
-    if (val.length <= 1) {
-        console.log(false);
-    } else {
-        console.log(true);
-    }
-
+    pass.addEventListener(evt, (e) => {
+        validate(pass, invalidPass, '#invalidPass', validateConstraint());
+        validate(passConf, invalidPassConf, '#invalidPassConf', confirmConstraint());
+    });
 });
 
 
 passConf.addEventListener('input', (e) => {
-
-    console.log(fName);
-    let pConf = pass.value;
-    let pConfVal = passConf.value;
-
-    if (pConf === pConfVal) {
-        console.log(true);
-    } else {
-        console.log(false);
-    }
-
+    validate(passConf, invalidPassConf, '#invalidPassConf', confirmConstraint());
 });
 
+function emailConstraint() {
+    let val = email.value;
+    return val.match(/^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i)
+}
 
+function validateConstraint() {
+    let val = pass.value;
+    return val.match((/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/))
+}
+
+function confirmConstraint() {
+    let pConf = pass.value;
+    let pConfVal = passConf.value;
+    return pConf === pConfVal;
+}
+
+function validate(inputType, invalidMessageType, invalidMessageTypeId, constraintType) {
+
+    if (constraintType) {
+        if ($(invalidMessageTypeId).length > 0) {
+            $(invalidMessageTypeId).remove();
+        }
+        if ($(inputType).hasClass('is-invalid')) {
+            $(inputType).removeClass('is-invalid');
+        }
+        //Prevents duplicate class additions
+        if (!$(inputType).hasClass('is-valid')) {
+            $(inputType).addClass("is-valid");
+        }
+    } else {
+        if (!$(invalidMessageTypeId).length > 0) {
+            $(inputType).after(invalidMessageType);
+        }
+        if ($(inputType).hasClass('is-valid')) {
+            $(inputType).removeClass('is-valid');
+        }
+        if (!$(inputType).hasClass('is-invalid')) {
+            $(inputType).addClass("is-invalid");
+        }
+    }
+}
