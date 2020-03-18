f0bb5969303be138f6a5776330396b35849adef1
Manu Puduvalli <28782438+manu-p-1@users.noreply.github.com>
2020-03-18 17:03:04 -0400

    + Added check for python version

--- start.sh
+++ start.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+cancel(){
+  read -r
+  exit
+}
+
 printf "BASH SCRIPT FOR NILE\n
 VERSION 1.0.0\n
 AUTHOR: MANU\n
@@ -31,15 +36,29 @@ then
   echo "INFO: niledb registered"
 else
   echo "ERROR: niledb could not be found on the MySQL instance"
-  read -r
-  exit
+  cancel
 fi
 
-printf "==ALL SQL CHECKS PASSED==\n\n"
+printf "==ALL SQL CHECKS PASSED==\n"
 
-ls
+#Using an actual python script in case people have python 2 on their computer too.
+version=$(python -c 'import sys; print("".join(map(str, sys.version_info[:3])))')
+re='^[0-9]+$'
+if [[ -z "$version" || $version =~ re ]];
+then
+    echo "PYTHON WAS NOT FOUND ON THE SYSTEM. NOW EXITING..."
+    cancel.
+fi
 
-printf "\n\n"
+verAsInt=${version//[\.]/}
+
+if ! [[ $verAsInt -ge 375 ]];
+then
+  echo "PYTHON VERSION IS $version BUT REQUIRES 3.7.5 OR HIGHER"
+  cancel
+fi
+
+printf "==ALL PYTHON CHECKS PASSED==\n\n"
 
 # DEBUG TRUE - FLASK RESTARTS FOR EVERY CHANGE :)
 export FLASK_DEBUG=1
@@ -48,4 +67,5 @@ flask run -h 127.0.0.1 &
 python -mwebbrowser http://127.0.0.1:5000
 
 read -r
+
 
