6a2709b6bfd7a51029106080b2dfc1a2cec72b95
manu-p-1 <mjp45770@uga.edu>
2020-03-22 18:48:13 -0400

    + Formatting

---
+++ static/user/js/browse.js
@@ -0,0 +1,6 @@
+console.log("███╗   ██╗██╗██╗     ███████╗\n");
+console.log("████╗  ██║██║██║     ██╔════╝\n");
+console.log("██╔██╗ ██║██║██║     █████╗\n");
+console.log("██║╚██╗██║██║██║     ██╔══╝  \n");
+console.log("██║ ╚████║██║███████╗███████╗\n");
+console.log("Hey there fellow developer.\nHope you have a great day!");
