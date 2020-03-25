e01584014696ea99f29e16c2edc8e629bf20a399
Manu Puduvalli <28782438+manu-p-1@users.noreply.github.com>
2020-03-24 21:30:31 -0400

    + Fixed tsconfig.json and ts files

---
+++ static/user/js/checkout.ts
@@ -0,0 +1,9 @@
+import {
+    emailConstraint,
+    INVALID_EMAIL_MSS, INVALID_F_NAME_MSS,
+    INVALID_L_NAME_MSS,
+    INVALID_STREET_MSS, INVALID_ZIP_MSS,
+    validator, zipCodeConstraint
+} from "./genericval.js";
+
+
