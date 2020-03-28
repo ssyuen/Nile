3e001157d5ecd3d4e1fe9af30b18ee8aeea2736d
Manu Puduvalli <28782438+manu-p-1@users.noreply.github.com>
2020-03-28 17:38:12 -0400

    - Removed Imports

--- static/user/js/checkout.ts
+++
@@ -1,9 +0,0 @@
-import {
-    emailConstraint,
-    INVALID_EMAIL_MSS, INVALID_F_NAME_MSS,
-    INVALID_L_NAME_MSS,
-    INVALID_STREET_MSS, INVALID_ZIP_MSS,
-    validator, zipCodeConstraint
-} from "./inputvalidation.js";
-
-
