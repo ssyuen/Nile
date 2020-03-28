2d32763f3b291533c4ab2cb96401fc94478f2442
Manu Puduvalli <28782438+manu-p-1@users.noreply.github.com>
2020-03-28 17:26:40 -0400

    + Password validation for user profile

---
+++ static/user/js/profile/pass.ts
@@ -0,0 +1,53 @@
+import {
+    InputValidationComplex, PURPOSE
+} from "../inputvalidation.js";
+
+const FORM: HTMLFormElement = document.getElementById("changePassForm") as HTMLFormElement;
+const PASS_CURR: HTMLInputElement = document.getElementById("currentPassword") as HTMLInputElement;
+const PASS_NEW: HTMLInputElement = document.getElementById("newPassword") as HTMLInputElement;
+const PASS_CONF: HTMLInputElement = document.getElementById("confirmNewPassword") as HTMLInputElement;
+
+const vc = new InputValidationComplex();
+
+FORM.addEventListener("submit", function (e) {
+
+
+    if (!vc.validateAll("#nameDetails")) {
+        e.preventDefault();
+        return;
+    }
+
+    //AJAX SUBMIT
+    /*
+        Make sure that the current password matched the one on the account, if not, flash an error message underneath
+        the #nameDetails element
+     */
+
+});
+
+
+Array<string>('input', 'focusin').forEach((evt: string) => {
+    PASS_NEW.addEventListener(evt, function () {
+        vc.setValidity(this, this, PURPOSE.Password, PURPOSE.Password.constraint(this.value));
+        vc.setValidity(PASS_CONF, PASS_CONF, PURPOSE.PasswordConfirmation, PURPOSE.PasswordConfirmation.constraint(this.value, PASS_CONF.value))
+    });
+
+    PASS_CONF.addEventListener(evt, function () {
+        let loc = "#inputLastnameGroup";
+        vc.setValidity(PASS_CONF, PASS_CONF, PURPOSE.PasswordConfirmation, PURPOSE.PasswordConfirmation.constraint(PASS_NEW.value, this.value))
+    });
+});
+
+//Make sure the User saved their Changes!!
+// @ts-ignore
+$("#accountListings").click(function (e: Event) {
+    let exit: boolean = true;
+    if (PASS_CURR.value.length) {
+        var conf = confirm("You have unsaved changes. Are you sure you want to continue?");
+        exit = conf;
+    }
+    if (!exit) {
+        e.preventDefault();
+        return false;
+    }
+});
