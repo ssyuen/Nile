2e59676b359f0a6d973c9d10fd41ee01e04a7b79
Glen George <36336744+glengeorge98@users.noreply.github.com>
2020-04-04 11:28:18 -0400

    New Script for Adding Books
    NileDB 4.5 Added

---
+++ MySQL Scripts/NileDB4.5.sql
@@ -0,0 +1,229 @@
+INSERT INTO book (ISBN, bindingID_book_FK, genreID_book_FK, title, price, numPages, nile_cover_ID, edition, publisher,
+                  publicationDate, stock, authorFirstName, authorLastName, summary)
+VALUES (
+	"9781338299144",
+        6,
+        2,
+        "Harry Potter and the Sorcerer's Stone",
+        6.89,
+        336,
+        "NILE_CI_0001",
+        NULL,
+        "Arthur A. Levine Books",
+        "2018-05-26",
+        500,
+        "J.K.",
+        "Rowling",
+        "Harry Potter has never been the star of a Quidditch team, scoring points while riding a broom far above the ground. He knows no spells, has never helped to hatch a dragon, and has never worn a cloak of invisibility."
+
+       );
+       
+INSERT INTO book (ISBN, bindingID_book_FK, genreID_book_FK, title, price, numPages, nile_cover_ID, edition, publisher,
+                  publicationDate, stock, authorFirstName, authorLastName, summary)       
+       
+VALUES (
+	"9781419729454",
+        7,
+        12,
+        "Diary of a Wimpy Kid",
+        13.06,
+        224,
+        "NILE_CI_0002",
+        "Cheesiest Edition",
+        "Harry N. Abrams",
+        "2017-08-08",
+        400,
+        "Jeff",
+        "Kinney",
+        "This Special CHEESIEST Edition is a must-have for longtime fans of the series and new readers alike. But before you open this book, you might want to cross your fingers—you wouldn’t want to get the Cheese Touch!"
+
+       );
+       
+INSERT INTO book (ISBN, bindingID_book_FK, genreID_book_FK, title, price, numPages, nile_cover_ID, edition, publisher,
+                  publicationDate, stock, authorFirstName, authorLastName, summary)
+                  
+VALUES (
+	"9780066238500",
+        6,
+        2,
+        "The Chronicles of Narnia",
+        17.99,
+        768,
+        "NILE_CI_0003",
+        NULL,
+        "HarperCollins",
+        "2001-10-01",
+        567,
+        "C.S.",
+        "Lewis",
+		"Epic battles between good and evil, fantastic creatures, betrayals, heroic deeds, and friendships won and lost all come together in this unforgettable world, which has been enchanting readers of all ages for over sixty years."
+
+       );
+       
+INSERT INTO book (ISBN, bindingID_book_FK, genreID_book_FK, title, price, numPages, nile_cover_ID, edition, publisher,
+                  publicationDate, stock, authorFirstName, authorLastName, summary)
+                  
+VALUES (
+	"9781119235538",
+        6,
+        8,
+        "Beginning Programming with Java For Dummies",
+        18.19,
+        560,
+        "NILE_CI_0004",
+        "5th Edition",
+        "For Dummies",
+        "2017-06-24",
+        347,
+        "Barry",
+        "Burd",
+		"Are you new to programming and have decided that Java is your language of choice? Are you a wanna-be programmer looking to learn the hottest lingo around? Look no further! Beginning Programming with Java For Dummies, 5th Edition is the easy-to-follow guide"
+        
+       );
+       
+INSERT INTO book (ISBN, bindingID_book_FK, genreID_book_FK, title, price, numPages, nile_cover_ID, edition, publisher,
+                  publicationDate, stock, authorFirstName, authorLastName, summary)
+                  
+VALUES (
+	"9781455525256",
+        7,
+        8,
+        "Gordon Ramsay's Home Cooking",
+        22.49,
+        320,
+        "NILE_CI_0005",
+        "2013 Edition",
+        "Grand Central Publishing",
+        "2013-04-09",
+        482,
+        "Gordon",
+        "Ramsay",
+		"Based on a new cooking show, this book will give experienced as well as novice cooks the desire, confidence and inspiration to get cooking."
+        
+       );
+       
+INSERT INTO book (ISBN, bindingID_book_FK, genreID_book_FK, title, price, numPages, nile_cover_ID, edition, publisher,
+                  publicationDate, stock, authorFirstName, authorLastName, summary)
+                  
+VALUES (
+	"9780760737934",
+        6,
+        11,
+        "Amazing Fantasy #15",
+        9.99,
+        245,
+        "NILE_CI_0006",
+        "2nd Impression Edition",
+        "Barnes & Noble",
+        "2003-01-01",
+        402,
+        "Stan",
+        "Lee",
+		"Collects the first appearance of Spider-Man in Amazing Fantasy 15 as well as the next 9 issues in his book. Artwork by Steve Ditko is different than many other marvel works as many of them were drawn by Jack Kirby."
+       
+       );
+       
+INSERT INTO book (ISBN, bindingID_book_FK, genreID_book_FK, title, price, numPages, nile_cover_ID, edition, publisher,
+                  publicationDate, stock, authorFirstName, authorLastName, summary)
+                  
+VALUES (
+	"9781984837158",
+        7,
+        12,
+        "The BFG",
+        14.99,
+        224,
+        "NILE_CI_0007",
+        NULL,
+        "Puffin Books ",
+        "2019-09-03",
+        522,
+        "Ronald",
+        "Dahl",
+		"The BFG is no ordinary bone-crunching giant. He is far too nice and jumbly. It's lucky for Sophie that he is."
+        
+       );
+       
+INSERT INTO book (ISBN, bindingID_book_FK, genreID_book_FK, title, price, numPages, nile_cover_ID, edition, publisher,
+                  publicationDate, stock, authorFirstName, authorLastName, summary)
+                  
+VALUES (
+	"9780785141570",
+        6,
+        11,
+        "Wolverine Vs. Hulk",
+        19.99,
+        144,
+        "NILE_CI_0008",
+        NULL,
+        "Marvel Comics",
+        "2017-01-01",
+        344,
+        "Stan",
+        "Lee",
+		"The First apperance of Wolverine. From the famous Incredible Hulk # 181."
+        
+       );
+       
+INSERT INTO book (ISBN, bindingID_book_FK, genreID_book_FK, title, price, numPages, nile_cover_ID, edition, publisher,
+                  publicationDate, stock, authorFirstName, authorLastName, summary)
+                  
+VALUES (
+	"9780786856299",
+        7,
+        2,
+        "The Lightning Thief: Percy Jackson",
+        11.89,
+        384,
+        "NILE_CI_0009",
+        NULL,
+        "Miramax Books/ Hyperion",
+        "2005-06-28",
+        502,
+        "Rick",
+        "Riordan",
+		"Percy Jackson is a good kid, but he can't seem to focus on his schoolwork or control his temper. And lately, being away at boarding school is only getting worse-Percy could have sworn his pre-algebra teacher turned into a monster and tried to kill him."
+        
+       );
+       
+INSERT INTO book (ISBN, bindingID_book_FK, genreID_book_FK, title, price, numPages, nile_cover_ID, edition, publisher,
+                  publicationDate, stock, authorFirstName, authorLastName, summary)
+                  
+VALUES (
+	"9780062420701",
+        7,
+        3,
+        "To Kill a Mockingbird",
+        18.74,
+        336,
+        "NILE_CI_0010",
+        "50th Anniversary Edition",
+        "Harper",
+        "2015-03-03",
+        592,
+        "Harper",
+        "Lee",
+		"Harper Lee's Pulitzer Prize-winning masterwork of honor and injustice in the deep South—and the heroism of one man in the face of blind and violent hatred"
+        
+       );
+       
+INSERT INTO book (ISBN, bindingID_book_FK, genreID_book_FK, title, price, numPages, nile_cover_ID, edition, publisher,
+                  publicationDate, stock, authorFirstName, authorLastName, summary)
+                  
+VALUES (
+	"9780374201234",
+        7,
+        8,
+        "The Mamba Mentality: How I Play",
+        21.55,
+        208,
+        "NILE_CI_0011",
+        NULL,
+        "MCD",
+        "2019-10-23",
+        200,
+        "Kobe",
+        "Bryant",
+		"The Mamba Mentality: How I Play is Kobe Bryant’s personal perspective of his life and career on the basketball court and his exceptional, insightful style of playing the game―a fitting legacy from the late Los Angeles Laker superstar."
+        
+       );
