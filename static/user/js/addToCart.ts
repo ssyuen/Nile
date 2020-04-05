a4518e6823bc307bb8280176e60f94faab68ea74
Manu Puduvalli <28782438+manu-p-1@users.noreply.github.com>
2020-04-05 00:02:18 -0400

    + Moved Script

---
+++ static/user/js/addToCart.ts
@@ -0,0 +1,40 @@
+$("#addToCartBtn").click(function () {
+    if ($(this).hasClass("blue-gradient")) {
+        addToCart(this);
+    } else {
+        removeFromCart(this);
+    }
+    $('#cartTotal').load(document.URL + ' #cartTotal')
+
+});
+
+function addToCart(btn) {
+    $(btn).removeClass("blue-gradient");
+    $(btn).addClass("btn-outline-success");
+    $(btn).children("span").html("Added to Cart");
+
+
+    $.ajax({
+        url: '/product/',
+        type: 'POST',
+        data: {'bookISBN': $("#bookISBN").attr("nile-isbn")}
+    });
+
+
+    console.log('posted');
+
+}
+
+function removeFromCart(btn) {
+    $(btn).removeClass("btn-outline-success");
+    $(btn).addClass("blue-gradient");
+    $(btn).children("span").html("Add to Cart");
+
+
+    $.ajax({
+        url: '/product/',
+        type: 'POST',
+        data: {'bookISBN': $("#bookISBN").attr("nile-isbn")}
+    });
+
+}
