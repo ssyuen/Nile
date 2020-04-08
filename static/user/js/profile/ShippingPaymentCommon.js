33a3a03b42f479ee2a2099913b5ae85519f2f863
Manu Puduvalli <28782438+manu-p-1@users.noreply.github.com>
2020-04-08 12:55:03 -0400

    + Added unversioned Shipping/Payment method JavaScript file

---
+++ static/user/js/profile/ShippingPaymentCommon.js
@@ -0,0 +1,6 @@
+export var PostFlags;
+(function (PostFlags) {
+    PostFlags["REMOVE"] = "REMOVE_FLAG";
+    PostFlags["EDIT"] = "EDIT_FLAG";
+    PostFlags["CREATE"] = "CREATE_FLAG";
+})(PostFlags || (PostFlags = {}));
