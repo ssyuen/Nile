7f1c9ad1806e4b0e8511954fb4446f89ce85ae3b
Lok-Kwong <46696970+Lok-Kwong@users.noreply.github.com>
2020-04-09 11:12:44 -0400

    Update managebooksform.js

--- static/admin/forms/managebooksform.js
+++ static/admin/forms/managebooksform.js
@@ -1,4 +1,4 @@
-import { InputValidationComplex, PURPOSE } from "../../user/js/inputvalidation.js";
+import { InputValidationComplex, PURPOSE } from "/static/admin/inputvalidation.js";
 let vc = new InputValidationComplex();
 const A_FNAME = document.getElementById("authorFirstName");
 const A_LNAME = document.getElementById("authorLastName");
