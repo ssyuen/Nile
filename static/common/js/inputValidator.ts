d5219ac229e1a598098ea7291009951d88c832a2
Manu Puduvalli <28782438+manu-p-1@users.noreply.github.com>
2020-04-09 19:47:31 -0400

    + Input validator parent class

---
+++ static/common/js/inputValidator.ts
@@ -0,0 +1,81 @@
+export interface IPurpose {
+    template: [string, string],
+    constraint: Function
+}
+
+export class InputValidator {
+
+    /*
+    Keeps track of each input element along with its validity.
+    On submit, every value of an input with the 'required' attribute
+    must be true.
+    */
+    curr_validity = new Map<string, boolean>();
+
+    public static scrollTopOfSelector(selectorPlace: string | HTMLElement | any): any {
+        $([document.documentElement, document.body]).animate({
+            scrollTop: $(selectorPlace).offset().top
+        }, 500);
+    }
+
+    public validateAll(selectorPlace: string | HTMLElement | any): boolean {
+        let errorHTML: string =
+            `<p class="text-center text-danger" id="somethingWrong">
+                One or more of the fields were incomplete or invalid
+            </p>`;
+
+        for (let key in this.curr_validity) {
+            let value: boolean = this.curr_validity[key];
+            let req: boolean = document.getElementById(key).hasAttribute('required');
+
+            if (req && !value) {
+                if (!$(selectorPlace).find("#somethingWrong").length) {
+                    $(selectorPlace).after(errorHTML);
+                }
+                InputValidator.scrollTopOfSelector(selectorPlace);
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public setValidity(elem: HTMLInputElement, loc: HTMLElement | string, purpose: IPurpose, constr: boolean) {
+        this.curr_validity[elem.id] = this.validator(elem, loc, purpose['template'], constr);
+    }
+
+    /*
+        Generalized validation function
+     */
+    private validator(inputType: HTMLElement,
+                      invalidMessageLocation: string | HTMLElement | any,
+                      invalidMessageType: [string, string],
+                      constraintType: (boolean | RegExpMatchArray)): boolean {
+
+        if (constraintType) {
+            if ($(invalidMessageLocation).next(invalidMessageType[1]).length) {
+                $(invalidMessageLocation).next(invalidMessageType[1]).remove();
+            }
+            if ($(inputType).hasClass('invalid')) {
+                $(inputType).removeClass('invalid');
+            }
+            //Prevents duplicate class additions
+            if (!$(inputType).hasClass('valid')) {
+                $(inputType).addClass("valid");
+            }
+            $(inputType).prop("aria-invalid", "false");
+            return true;
+        } else {
+            if (!$(invalidMessageLocation).next(invalidMessageType[1]).length) {
+                $(invalidMessageLocation).after(invalidMessageType[0]);
+            }
+            if ($(inputType).hasClass('valid')) {
+                $(inputType).removeClass('valid');
+            }
+            if (!$(inputType).hasClass('invalid')) {
+                $(inputType).addClass("invalid");
+            }
+            $(inputType).prop("aria-invalid", "true");
+            return false;
+        }
+    }
+}
