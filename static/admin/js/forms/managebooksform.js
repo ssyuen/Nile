8460809af4700923d733004f551416359c060701
Manu Puduvalli <28782438+manu-p-1@users.noreply.github.com>
2020-04-10 11:48:01 -0400

    + Fixed: Added unversioned code

--- static/admin/js/forms/managebooksform.js
+++ static/admin/js/forms/managebooksform.js
@@ -1,3 +1,38 @@
+import {AdminInputValidator, PURPOSE} from "../adminInputValidation.js";
+
 $("#editBtn").click(function () {
     $("#manageBooksForm input, #manageBooksForm select, #manageBooksForm textarea").removeAttr("readonly disabled");
+});
+let vc = new AdminInputValidator(5);
+const A_FNAME = document.getElementById("authorFirstName");
+const A_LNAME = document.getElementById("authorLastName");
+const BOOK_TITLE = document.getElementById("bookTitle");
+const ISBN = document.getElementById("isbn");
+const PUBLISHER = document.getElementById("publisher");
+const FORM = document.getElementById("manageBooksForm");
+// GOES THROUGH EACH INPUT AND CHECKS
+Array('input', 'focusin').forEach((evt) => {
+    console.log("INSIDE MANAGE BOOKS");
+    // CHECK BOOK_TITLE
+    BOOK_TITLE.addEventListener(evt, function () {
+        vc.setValidity(this, this, PURPOSE.TITLE, PURPOSE.TITLE.constraint(this.value));
+    });
+    A_FNAME.addEventListener(evt, function () {
+        vc.setValidity(this, this, PURPOSE.Firstname, PURPOSE.Firstname.constraint(this.value));
+    });
+    A_LNAME.addEventListener(evt, function () {
+        vc.setValidity(this, this, PURPOSE.Lastname, PURPOSE.Lastname.constraint(this.value));
+    });
+    ISBN.addEventListener(evt, function () {
+        vc.setValidity(this, this, PURPOSE.ISBN, PURPOSE.ISBN.constraint(this.value));
+    });
+    PUBLISHER.addEventListener(evt, function () {
+        vc.setValidity(this, this, PURPOSE.PUBLISHER, PURPOSE.PUBLISHER.constraint(this.value));
+    });
+});
+$(FORM).on("submit", function (e) {
+    if (!vc.validateAll('.card-title')) {
+        e.preventDefault();
+        return false;
+    }
 });
