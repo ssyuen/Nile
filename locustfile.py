4f9dd639b3fec0f87200c0c8504936775341ce90
ssyuen <samuel.s.yuen@gmail.com>
2020-04-12 17:12:54 -0400

    added skeleton to locust

--- locustfile.py
+++ locustfile.py
@@ -1,4 +1,22 @@
-from locust import HttpLocust, between
+from locust import HttpLocust, between, TaskSet, task
 
-class WebsiteUser(HttpLocust):
-	pass
+
+class Register(TaskSet):
+    DEFAULT_REGISTER_INPUTS = ['inputFirstName',
+                               'inputLastName', 'inputEmail', 'inputPassword']
+    SHIPPING_INPUTS = ['addAddressStreetAddress', 'addAddressApartmentOrSuite',
+                       'addZipcode', 'addAddressCity', 'addAddressState', 'addAddressCountry']
+    PAYMENT_INPUTS = ['cardHolderFirstName', 'cardHolderLastName', 'ccn', 'ccexp', 'CCNProvider', 'billingStreetAddress',
+                      'billingApartmentOrSuite', 'billingZipcode', 'billingCity', 'billingState', 'billingCountry']
+    @task
+    def register(self):
+        self.client.post('/register/', {'inputFirstName': ''})
+
+
+class Login(TaskSet):
+    pass
+
+
+class RegisterUser(HttpLocust):
+    task_set = Register
+    wait_time = between(5, 10)
