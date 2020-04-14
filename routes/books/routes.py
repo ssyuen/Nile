66106a40f8f32e46a6490e17daba7bd2b737968e
ssyuen <samuel.s.yuen@gmail.com>
2020-04-14 17:18:42 -0400

    fixed search bar error

--- routes/books/routes.py
+++ routes/books/routes.py
@@ -9,7 +9,7 @@ books_bp = Blueprint('books_bp', __name__,
                      template_folder='templates', static_folder='static')
 
 
-@books_bp.route('/api/books/', methods=['GET'])
+@books_bp.route('/api/books/isbn/', methods=['GET'])
 def query_isbn(search_query=None):
     if request.args is None:
         return redirect(url_for('common_bp.landing_page'))
@@ -85,8 +85,7 @@ def query_books(search_query=None):
 
                     header = [desc[0] for desc in cursor.description]
                     payload = [dict(zip(header, result)) for result in results]
-                    books = [book for book in payload if search_query.lower() in str(
-                        book.values()).lower()]
+                    books = [book for book in payload if search_query.lower() in str(book.values()).lower()]
 
                     if request.args['search_query'].lower() == 'test':
                         return jsonify(payload)
@@ -119,7 +118,9 @@ def query_books(search_query=None):
                 conn.close()
                 return render_template('browse.html', books=books, genres=genres, genre_counts=genre_counts, bindings=bindings, binding_counts=binding_counts)
             except pymysql.Error as e:
+                print(e)
                 return redirect(url_for('common_bp.landing_page'))
 
         else:
+            print('here')
             return redirect(url_for('common_bp.landing_page'))
