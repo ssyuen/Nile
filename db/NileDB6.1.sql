b3513cd282a2a8a506aca3cb8bcee0d68d13fadf
Manu Puduvalli <28782438+manu-p-1@users.noreply.github.com>
2020-04-15 13:02:25 -0400

    - Removed NileDB6.0 scripts

--- db/NileDB6.1.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE `niledb`.`book_orderdetail`
-    DROP PRIMARY KEY,
-    ADD PRIMARY KEY (`userID_bod_FK`, `ISBN_bod_FK`),
-    ADD INDEX `id` (`id` ASC) VISIBLE;
-ALTER TABLE `niledb`.`book_orderdetail` RENAME INDEX `usserID_bod_FK_idx` TO `userID_bod_FK_idx`;
-ALTER TABLE `niledb`.`book_orderdetail`
-    ALTER INDEX `userID_bod_FK_idx` INVISIBLE;
--- db/NileDB6.2.sql
+++
@@ -1,5 +0,0 @@
-alter table promotion
-    add name varchar(50) not null after id;
-
-alter table promotion
-    add notes varchar(500) not null;
