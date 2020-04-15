721b211c2fecad626eff77cf551f9d48b1c2f69b
ssyuen <samuel.s.yuen@gmail.com>
2020-04-15 10:39:18 -0400

    + now tracking subscription files

---
+++ static/user/js/profile/subscription.js
@@ -0,0 +1,24 @@
+var emailSubscription = document.getElementById("emailSubscription");
+$(emailSubscription).change(function () {
+    var flag;
+    if (this.checked) {
+        var returnVal = confirm("Are you sure?");
+        $(this).prop("checked", returnVal);
+        flag = email.SUBSCRIBE;
+    }
+    else {
+        var returnVal = confirm("Are you sure?");
+        $(this).prop("unchecked", returnVal);
+        flag = email.UNSUBSCRIBE;
+    }
+    $.ajax({
+        url: '/subscriptions/',
+        type: 'POST',
+        data: { 'flag': flag }
+    });
+});
+var email;
+(function (email) {
+    email["SUBSCRIBE"] = "SUBSCRIBE";
+    email["UNSUBSCRIBE"] = "UNSUBSCRIBE";
+})(email || (email = {}));
