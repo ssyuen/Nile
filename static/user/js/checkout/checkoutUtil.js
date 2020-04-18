7805627282cc0cc49ff7336bac35e0cc1985cc3e
Manu Puduvalli <28782438+manu-p-1@users.noreply.github.com>
2020-04-18 12:34:25 -0400

    + Added unversioned checkoutUtil.js file

---
+++ static/user/js/checkout/checkoutUtil.js
@@ -0,0 +1,21 @@
+export function serializedToObject(items) {
+    var data = {};
+    $(items).serializeArray().map(function (x) {
+        data[x.name] = x.value;
+    });
+    return data;
+}
+export function post(path, method = "POST", ...parameters) {
+    let form = $('<form></form>');
+    form.attr({ "method": method, "action": path });
+    $.each(parameters, function (key, value) {
+        let field = $('<input></input>');
+        field.attr("type", "hidden");
+        field.attr("name", key);
+        field.attr("value", value);
+        form.append(field);
+    });
+    // Form NEEDS to be in th document body to submit form
+    $(document.body).append(form);
+    form.submit();
+}
