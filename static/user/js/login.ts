8c54025993302d96fe5fb728c9d13eb3da6b2731
Manu Puduvalli <28782438+manu-p-1@users.noreply.github.com>
2020-04-18 12:40:21 -0400

    + Removed old login.ts

--- static/user/js/login.ts
+++
@@ -1,9 +0,0 @@
-$("#loginForm").on("submit", function () {
-    let ref = $("#loginBtn");
-    ref.html(`<i class="fas fa-spinner fa-pulse"></i>`);
-    ref.prop("disabled", true);
-});
-
-$("#loginBtn").on("click", function () {
-    $("loginForm").submit();
-});
