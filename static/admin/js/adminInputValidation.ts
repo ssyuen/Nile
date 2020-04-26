3d2cb692be1ca980371d1be43c7bc7075025c009
Lok Kwong <hk65962@uga.edu>
2020-04-26 16:08:29 -0400

    ts

--- static/admin/js/adminInputValidation.ts
+++ static/admin/js/adminInputValidation.ts
@@ -12,37 +12,37 @@ export class AdminInputValidator extends InputValidator {
 
     static INVALID_PASS_MSS: [string, string] = [`
         <small class="text-danger error-messsage" id="invalidPass">
-          Password must be more than 8 or more characters long, contain at least 1 uppercase character, 1 lowercase character, and 1 number.
+          Password must be more than 8 or more characters long, contain at least 1 uppercase character, 1 lowercase character, and 1 number
         </small>
 `, "#invalidPass"];
     static INVALID_PASS_CONF_MSS: [string, string] = [`
         <small class="text-danger error-message" id="invalidPassConf">
-          Passwords do not match.
+          Passwords do not match
         </small>
 `, "#invalidPassConf"];
     static INVALID_F_NAME_MSS: [string, string] = [`
         <small class="text-danger error-message" id="invalidFName">
-          Firstname must be more than 1 character.
+          Firstname must be more than 1 character
         </small>    
 `, "#invalidFName"];
     static INVALID_L_NAME_MSS: [string, string] = [`
         <small class="text-danger error-message" id="invalidLName">
-          Lastname must be more than 2 characters.
+          Lastname must be more than 2 characters
         </small>
 `, "#invalidLName"];
     static INVALID_BOOK_TITLE_MSS: [string, string] = [`
         <small class="text-danger error-message" id="invalidTitle"">
-            Title needs 1 or more characters.
+            Title needs 1 or more characters
         </small>
 `, "#invalidTitle"];
     static INVALID_ISBN_MSS: [string, string] = [`
         <small class="text-danger error-message" id="invalidISBN"">
-            Must have 13 digits and be in the format 1234567891011 or 978-0-596-52068-7.
+        Must have 13 digits and be in the format 1234567891011, 978-0-596-52068-7, or 978-0735219090
         </small>
 `, "#invalidISBN"];
     static INVALID_PUBLISHER_MSS: [string, string] = [`
         <small class="text-danger error-message" id="invalidPublisher"">
-          Publisher needs to be 2 or more characters.
+          Publisher needs to be 2 or more characters
         </small>
 `, "#invalidPublisher"];
 
