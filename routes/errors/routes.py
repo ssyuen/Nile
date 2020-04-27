d529055922da4e0acf6e123a70c3f9a8873d4192
Manu Puduvalli <28782438+manu-p-1@users.noreply.github.com>
2020-04-27 01:25:09 -0400

    + Added: Error Pages

--- routes/errors/routes.py
+++ routes/errors/routes.py
@@ -1,8 +1,8 @@
 from flask import render_template
 def page_not_found(e):
-    # return render_template('404.html'),404
+    return render_template('errors/404.html'), 404
     pass
 
 def logic_error(e):
-    # return render_template('500.html'),500
+    return render_template('errors/500.html'), 500
     pass
