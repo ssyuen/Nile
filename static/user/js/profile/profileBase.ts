853fcd7d7c2c84625e43bfb650761c79d468d579
Manu Puduvalli <28782438+manu-p-1@users.noreply.github.com>
2020-04-27 01:02:16 -0400

    + Fixed: Removed unnecessary console.log

--- static/user/js/profile/profileBase.ts
+++ static/user/js/profile/profileBase.ts
@@ -2,7 +2,6 @@ $(document).scroll(function () {
     if ($(window).width() >= 992) {
         var y = $(document).scrollTop(),
             header = $("#accountListings");
-        console.log(y);
         if (y >= (y + $('#mainContent').offset().top) || Math.floor(y) <= 230) {
             header.css({"position": "static", "width": "auto"});
             $("#scrollWelcome").hide('fast', function () {
