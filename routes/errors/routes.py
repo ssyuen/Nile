91f683fc12b3f011ce9d7cf8fa24ecf5e6db84a0
ssyuen <samuel.s.yuen@gmail.com>
2020-04-26 23:19:49 -0400

    added error handlers

--- routes/errors/routes.py
+++ routes/errors/routes.py
@@ -1,6 +1,8 @@
 from flask import render_template
 def page_not_found(e):
+    # return render_template('404.html'),404
     pass
 
 def logic_error(e):
+    # return render_template('500.html'),500
     pass
